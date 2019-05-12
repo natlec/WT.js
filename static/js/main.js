e0eb9c1f9dd13adcc1ab652c09bf2c662233470a
natlec <accounts@natlec.com>
2019-05-12 21:55:26 +1000

    Apply general cart style

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+/*** 
+	Copyright 2019, Nathan Lecompte 
+		ID: 45423725
+***/
+
+$(document).ready(function() {
+      
+})
--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-
-   $('#message').html("This text has been inserted using jQuery from /static/js/script.js")
-
-})
-
